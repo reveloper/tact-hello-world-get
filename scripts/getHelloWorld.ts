aefbcf36295ea01da5c5390cb8e6712d8ef5dc4b
AlexG <39581753+Reveloper@users.noreply.github.com>
2023-08-29 15:44:26 +0300

    added_description

--- scripts/getHelloWorld.ts
+++ scripts/getHelloWorld.ts
@@ -1,22 +1,24 @@
-import { toNano } from 'ton-core';
+import { toNano, Address } from 'ton-core';
 import { HelloWorld } from '../wrappers/HelloWorld';
 import { NetworkProvider } from '@ton-community/blueprint';
 
-export async function run(provider: NetworkProvider) {
-    const helloWorld = provider.open(await HelloWorld.fromInit());
 
-    await helloWorld.send(
-        provider.sender(),
-        {
-            value: toNano('0.05'),
-        },
-        {
-            $$type: 'Deploy',
-            queryId: 0n,
-        }
-    );
+export async function run(provider: NetworkProvider, args: string[]) {
 
-    await provider.waitForDeploy(helloWorld.address);
+    //regular code snippet from increment connection
+    const ui = provider.ui();
 
-    // run methods on `helloWorld`
+    const address = Address.parse(args.length > 0 ? args[0] : await ui.input('Hello world address'));
+
+    if (!(await provider.isContractDeployed(address))) {
+        ui.write(`Error: Contract at address ${address} is not deployed!`);
+        return;
+    }
+    //connection done
+
+    const helloWorld = provider.open(HelloWorld.fromAddress(address)); // make contract entity via open()
+
+    const greeting = await helloWorld.getGreeting(); // retrieve data via get method generated int tact.ts sources
+
+    console.log(greeting);
 }
